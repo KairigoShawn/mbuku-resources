63be7e8aff0a9e046fe98727e5c6b5f126eec846
Aisha-Farah <aisha.farah@student.moringaschool.com>
2021-09-23 16:58:54 +0300

    add nav-bar

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const books = document.querySelectorAll('.book');
 
 const bookPickup = [
-  'Book is currently available for pick up. Opening hours 0900 - 1700hrs.',
+  'Book is currently available for pick up. Opening hours 09:00 am - 17:00 pm.',
   'Book is currently unavailable! We will notify you when the book is available.',
 ];
 
